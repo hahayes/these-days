ea8c41d42e77466a10edd9047111616852d422c5
mobeets <mobeets@gmail.com>
2014-08-26 16:41:13 -0500

    random phrase

--- TheseDays.py
+++ TheseDays.py
@@ -1,25 +1,29 @@
 import rumps
+from phrase import load, phrase
+words = load()
 
 rumps.debug_mode(True)
-
 class TheseDaysApp(rumps.App):
     def __init__(self):
-        super(TheseDaysApp, self).__init__("These Days")
-        self.menu = ['Preferences', 'Option', 'Say hi']
-        self.icon = 'icon.png'
-        self.title = None
+        super(TheseDaysApp, self).__init__("These Days", title=None, icon='icon.png')
+        self.menu = ["These days...", rumps.separator, "Auto-ask", "Preferences"]
 
     @rumps.clicked("Preferences")
     def prefs(self, _):
-        rumps.alert("jk! no preferences available!")
+        rumps.alert("None yet!")
 
-    @rumps.clicked("Option")
+    @rumps.clicked("Auto-ask")
     def onoff(self, sender):
         sender.state = not sender.state
 
-    @rumps.clicked("Say hi")
+    @rumps.clicked("These days...")
     def sayhi(self, _):
-        rumps.notification("Title", "Subtitle", "helloooooooo")
+        self.window = rumps.Window(dimensions=(300, 100))
+        self.window.message = "Whatcha been doing lately?"
+        self.window.default_text = phrase(words)
+        self.window.icon = self.icon
+        self.response = self.window.run()
+        # rumps.alert("These days you've been...", self.response.text)
 
 if __name__ == "__main__":
     TheseDaysApp().run()
---
+++ phrase.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+FILENAME = 'words.json'
+
+def dump(outfile=FILENAME):
+    from pattern.en import wordnet, conjugate, pluralize
+    from pattern.en.wordlist import BASIC
+    basic_words = lambda pos, fcn=(lambda x: x): [fcn(w) for w in BASIC if wordnet.synsets(w, pos=pos)]
+    lkp = {'NOUNS': basic_words('NNS', pluralize), 'VERBS': basic_words('VB', lambda x: conjugate(x, 'part')), 'ADJS': basic_words('JJ')}
+    json.dump(lkp, open(outfile, 'w'))
+
+def load(infile=FILENAME):
+    return json.load(open(infile))
+
+def phrase(lkp):
+    return '{0} the {1}'.format(random.choice(lkp['VERBS']), random.choice(lkp['NOUNS']))
+
+if __name__ == '__main__':
+    dump()
